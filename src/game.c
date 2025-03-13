8ed81fd01cf991dcc8caf892b525e91b020bae90
isaacdenny <isaacquinndenny@gmail.com>
2025-03-13 10:02:26 -0400

    plan for validate board

---
+++ src/game.c
@@ -0,0 +1,10 @@
+#include "game.h"
+
+void validate_board(uint64_t* ships, game_data* gameData) {
+  // go over every ship and:
+  // - check for collisions
+  // - make sure there's no "wrapping"
+  // - make sure lengths are correct
+  //
+  // build board, fill gameData struct 
+}
